a6b2b612dae12c881a05b4083caa96098aa4d25a
Ali <as598102@icloud.com>
2020-11-26 00:48:10 +0200

    first commit

---
+++ Arab News /Arab News/Conroller/DetailViewController.swift
@@ -0,0 +1,27 @@
+
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    var urllink:String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lable1.text=urllink
+        
+        if NSURL(string: urllink!) != nil {
+        webView.load(NSURLRequest(url: NSURL(string: urllink!)! as URL) as URLRequest)
+        } else {
+            print ("URL not found")
+        }
+        
+    }
+    
+    @IBOutlet weak var lable1: UILabel!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+
+}
---
+++ Arab News /Arab News/Model/NewAPI.swift
@@ -0,0 +1,51 @@
+import Foundation
+
+
+protocol NewsAPIDelegate {
+    func didFetchPosts (posts: Posts)
+    func didFailWithError (error: Error?)
+}
+
+struct NewsAPI {
+    
+    
+    var delegate: NewsAPIDelegate?
+    
+    func fetchNews (){
+        
+        let urlString="https://newsapi.org/v2/top-headlines?sources=google-news-sa&apiKey=2524838879e349c8a7683c7496112fda"
+        
+        let url=URL(string: urlString)
+        let urlSession=URLSession(configuration: .default)
+        let task=urlSession.dataTask(with: url!) { (data, urlResponse, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            }else {
+                
+                do {
+                    let thisPosts = try JSONDecoder().decode(Posts.self, from: data!)
+                    
+                    delegate?.didFetchPosts(posts: thisPosts)
+
+                } catch
+                {
+                    print (error)
+                }
+                
+                
+                
+                
+                
+            }
+            
+
+        }
+        
+        task.resume()
+        
+        
+        
+        
+    }
+    
+}
